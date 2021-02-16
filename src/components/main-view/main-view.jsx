b8e02b855366b2c10a05196602a4face62872b8d
iceyisaak <me@iceyisaak.com>
2021-02-16 17:30:15 +0100

    Added Back Button

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 
+// Import all the children components to be used
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+
 export class MainView extends React.Component {
 
+  // Construct the component
   constructor() {
+
+    // Call on React.Component
     super();
 
+    // Initialse the states for this component
     this.state = {
       movies: null,
       selectedMovie: null
     };
   }
 
+  // When Component is mounted to the DOM
   componentDidMount(){
 
         // Get Data from API
@@ -36,7 +43,10 @@ export class MainView extends React.Component {
           });
       }
 
+  // Function to Select Movie: Take in the 'movie data'
   onMovieClick(movie) {
+
+    // Set selectedMovie to that 'movie data'
     this.setState({
       selectedMovie: movie
     });
@@ -44,21 +54,38 @@ export class MainView extends React.Component {
 
 
   render() {
+
+    // Destructure the states
     const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
+    // Return the component
     return (
      <div className="main-view">
-      {selectedMovie
-         ? <MovieView
-            movie={selectedMovie}
-          />
-         : movies.map(movie => (
+
+       
+      {
+      // If a movie is selected, Return that selected <MovieView/>
+      selectedMovie ?
+        <MovieView
+          movie={selectedMovie}
+        />
+
+        //Else 
+         :
+          // Return a list of <MovieCard/>
+         movies.map(movie => (
            <MovieCard 
+
+            // Assign a unique key to each <MovieCard/> using 'movie._id'  
             key={movie._id} 
+
+            // Pass the 'movie data' as prop to each <MovieCard/>
             movie={movie} 
+
+            // When <MovieCard/> is clicked, pass that 'movie data' to this function
             onClick={
               movie => this.onMovieClick(movie)
             }
