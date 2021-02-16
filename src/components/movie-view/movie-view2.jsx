b0e1a2d349c76c9fb7b5188dcd5b15cd5b02bc34
iceyisaak <me@iceyisaak.com>
2021-02-16 15:14:20 +0100

    Examining errors

--- src/components/movie-view/movie-view2.jsx
+++ src/components/movie-view/movie-view2.jsx
@@ -1,74 +1,62 @@
 import React from 'react';
-import axios from 'axios';
 
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../main-view/main-view';
+export class MovieView extends React.Component{
 
-export class MainView extends React.Component {
-
-  // Initialise all the states to be used in the component
-  constructor() {
+  constructor(){
     super();
-
-    this.state = {
-      movies: null,
-      selectedMovie: null
-    };
-  }
- 
-  // When component is inserted into DOM
-  componentDidMount(){
-
-    // Get Data from API
-    axios.get('https://myflix-20210211.herokuapp.com/movies/')
-
-    // Then bring in the response from server
-    .then(
-      response => {
-          // Assign result to the state
-          this.setState({
-            movies: response.data
-          });
-        }
-      )
-      .catch(
-        (error) => {
-        console.log(error);
-      });
-  }
-
-  onMovieClick(movie){
-    this.setState({
-      selectedMovie: movie
-    });
+    this.state = {};
   }
 
   render(){
 
-    const { movies, selectedMovie } = this.state;
+    const {movie} = this.props;
 
-    if(!movies) return <div className="main-view"></div>;
+    if(!movie) {
+      return null;
+    }
 
-    return(
-      <div className="main-view">
-        {selectedMovie 
-          ? <MovieView 
-              movie={selectedMovie} 
-            />
-          : movies.map(
-            movie => (
-              <MovieCard 
-                className="movie-card" 
-                key={movie._id}
-                movie={movie}
-                onClick={
-                  movie => this.onMovieClick(movie)
-                }
-              />
-            )
-          )
-        }
+    return (
+
+      <div className="movie-view">
+        <img 
+         className="movie-poster"
+         src={movie.ImagePath}
+         alt="Movie Poster" 
+         />
+         <div className="movie-title">
+           <span className="label">
+             Title: 
+           </span>
+           <span className="value">
+             {movie.Title}
+           </span>
+         </div>
+         <div className="movie-description">
+           <span className="label">
+             Description:
+           </span>
+           <span className="value">
+             {movie.Description}
+           </span>
+         </div>
+         <div className="movie-genre">
+           <span className="label">
+             Genre: 
+           </span>
+           <span className="value">
+             {movie.Genre.Name}
+           </span>
+         </div>
+         <div className="movie-director">
+           <span className="label">
+             Director:
+           </span>
+           <span className="value">
+             {movie.Director.Name}
+           </span>
+         </div>
       </div>
     );
   }
+
 }
