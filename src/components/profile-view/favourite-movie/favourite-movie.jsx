e50aa6f1b71b69c59eefe5e4b87d59f3ba6a8397
iceyisaak <me@iceyisaak.com>
2021-03-10 08:35:39 +0100

    Debugged FavouriteMovies view

--- src/components/profile-view/favourite-movie/favourite-movie.jsx
+++ src/components/profile-view/favourite-movie/favourite-movie.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 const FavouriteMovie = ({
   userInfo,
--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ProfileInfo from './profile-info/profile-info';
 import FavouriteMovie from './favourite-movie/favourite-movie';
-import ProfileAccount from './profile-account/profile-account';
 
 
 import './profile-view.scss';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Nav from 'react-bootstrap/Nav';
 
 export class ProfileView extends React.Component{
 
@@ -35,7 +33,6 @@ export class ProfileView extends React.Component{
                 userInfo={userInfo}
                 movies={movies}
               />
-              abc
             </Tab>
             <Tab></Tab>
           </Tabs>
